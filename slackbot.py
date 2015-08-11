48c7a2e67e077dd6c984a07a16bcd27169d6fad4
Per-Gustaf Stenberg <per-gustaf.stenberg@stonepath.se>
2015-08-11 10:09:47 +0200

    added hostname in notification

--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, json, requests, os
+import sys, json, requests, os, socket
 
 consul_data = json.loads(sys.stdin.read())
 
@@ -11,6 +11,7 @@ print "Channel: %s" % os.environ.get('SLACK_CHANNEL')
 slackbot_url = "https://%s.slack.com/services/hooks/slackbot?token=%s&channel=%s" % (str(os.environ.get('SLACK_TEAM')), str(os.environ.get('SLACK_TOKEN')), str('%23' + os.environ.get('SLACK_CHANNEL')))
 
 pending_resolutions = []
+hostname = socket.gethostname()
 
 def send_post(message):
     global slackbot_url
@@ -21,13 +22,13 @@ def send_post(message):
 for data_entry in consul_data:
     for check in data_entry['Checks']:
         if(check['Status'] == 'passing' and check['CheckID'] in pending_resolutions):
-            slackbot_message = "_%s_ seams on *%s* (%s) to be resolved:\n```%s```" % (check['Name'], data_entry['Node']['Node'], data_entry['Node']['Address'], check['Output'])
+            slackbot_message = "%s:_%s_ seams on *%s* (%s) to be resolved:\n```%s```" % (hostname, check['Name'], data_entry['Node']['Node'], data_entry['Node']['Address'], check['Output'])
             print "%s@%s=>passing:" % (check['Name'],data_entry['Node']['Node'])
             send_post(slackbot_message)
             pending_resolutions.remove("%s@%s" % (check['CheckID'],data_entry['Node']['Node']))
 
         elif(check['Status'] == 'warning' and not check['CheckID'] in pending_resolutions):
-            slackbot_message = "Ups! _%s_ failed on *%s* (%s):\n```%s```" % (check['Name'], data_entry['Node']['Node'], data_entry['Node']['Address'], check['Output'])
+            slackbot_message = "%s:Ups! _%s_ failed on *%s* (%s):\n```%s```" % (hostname, check['Name'], data_entry['Node']['Node'], data_entry['Node']['Address'], check['Output'])
             print "%s@%s=>warning:" % (check['Name'],data_entry['Node']['Node'])
             send_post(slackbot_message)
             pending_resolutions.append("%s@%s" % (check['CheckID'],data_entry['Node']['Node']))
